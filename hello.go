812c6176bad71b11851eec3ce12680fa8f56ea01
Mehrad <mehrad.arashrad@gmail.com>
2016-04-21 16:04:50 +1000

    init commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Mehradzie/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("Mehrad"))
+	_ = "breakpoint"
+	var foo string = "half a day"
+	fmt.Printf(foo)
+	fmt.Printf(" hello, world!\n")
+}
